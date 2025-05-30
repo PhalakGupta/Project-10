32d4cd61abd7b568d554e11c5606ef6b5d562d9e
PhalakGupta <phalakprincess22@gmail.com>
2025-05-30 18:31:04 +0530

    Add files via upload

---
+++ project-10.py
@@ -0,0 +1,7 @@
+age = int(input ("Enter your age:"))
+if age >= 10 and age <= 20:
+    print ("You are between 10-20!")
+elif age > 20:
+    print ("You are older than 20!")
+else:
+    print ("You are younger than 10!")
